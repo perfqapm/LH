9f69690a4a266cc606e5e77c6e57636bd0e2d8f0
perfqapm <133971445+perfqapm@users.noreply.github.com>
2025-03-18 14:04:22 +0530

    Update lighthouse.mjs

--- lighthouse.mjs
+++ lighthouse.mjs
@@ -5,6 +5,7 @@ import fs from 'fs';
 
 globalThis.fetch = fetch;
 let performanceScore;
+let firstContentfulPaint;
 
 async function generateLighthouseReport(url) {
   const chrome = await launch({ chromeFlags: ['--headless'] });
@@ -23,21 +24,29 @@ async function generateLighthouseReport(url) {
 
   performanceScore = runnerResult.lhr.categories.performance.score * 100;
   console.log('Lighthouse Performance Score:', performanceScore);
+
+  firstContentfulPaint = runnerResult.lhr.audits['first-contentful-paint'].displayValue;
+  console.log('Lighthouse First Contentful Paint:', firstContentfulPaint);
+  
   // Read the existing .env file
   const envFilePath = '.env';
   const envFileContent = fs.readFileSync(envFilePath, 'utf8');
+  
   // Define the dynamic variable and its value
   const dynamicVariableName = 'performanceScore';
   const dynamicVariableValue = performanceScore;
+  const dynamicVariableName1 = 'firstContentfulPaint';
+  const dynamicVariableValue1 = firstContentfulPaint;
+  
   // Create or update the dynamic variable in the .env file content
   let updatedEnvFileContent = '';
-  if (envFileContent.includes(dynamicVariableName)) {
+  if (envFileContent.includes(dynamicVariableName) || envFileContent.includes(dynamicVariableName1)) {
   // If the variable already exists, update its value
-  const regex = new RegExp(`${dynamicVariableName}=.*`);
-  updatedEnvFileContent = envFileContent.replace(regex, `${dynamicVariableName}=${dynamicVariableValue}`);
+  const regex = new RegExp(`${dynamicVariableName}=.*\n${dynamicVariableName1}=.*\n${dynamicVariableName2}=.*`);
+  updatedEnvFileContent = envFileContent.replace(regex, `${dynamicVariableName}=${dynamicVariableValue}\n${dynamicVariableName1}=${dynamicVariableValue1}`);
   } else {
   // If the variable does not exist, append it to the content
-  updatedEnvFileContent = `${envFileContent}\n${dynamicVariableName}=${dynamicVariableValue}`;
+  updatedEnvFileContent = `${envFileContent}\n${dynamicVariableName}=${dynamicVariableValue}\n${dynamicVariableName1}=${dynamicVariableValue1}`;
   }
   // Write the updated .env file content
   fs.writeFileSync(envFilePath, updatedEnvFileContent);
@@ -45,7 +54,7 @@ async function generateLighthouseReport(url) {
   console.log('Lighthouse Seo Score:', runnerResult.lhr.categories.seo.score * 100);
   console.log('Lighthouse best-practices Score:', runnerResult.lhr.categories['best-practices'].score * 100);
   
-  console.log('Lighthouse First Contentful Paint:', runnerResult.lhr.audits['first-contentful-paint'].displayValue);
+  
   console.log('Lighthouse Largest Contentful Paint:', runnerResult.lhr.audits['largest-contentful-paint'].displayValue);
   console.log('Lighthouse Total Blocking Time:', runnerResult.lhr.audits['total-blocking-time'].displayValue);
   console.log('Lighthouse Cumulative Layout Shift:', runnerResult.lhr.audits['cumulative-layout-shift'].displayValue);
